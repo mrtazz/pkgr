d9127a3d42849c9414737f9c968990299295fc79
Daniel Schauenberg <d@unwiredcouch.com>
2018-08-25 20:30:57 +0200

    add --version flag

--- cmd/pkgr/main.go
+++ cmd/pkgr/main.go
@@ -2,17 +2,30 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"github.com/mrtazz/pkgr/archiver"
 	"github.com/mrtazz/pkgr/manifest"
 	"log"
+	"os"
+)
+
+var (
+	version   = ""
+	builder   = ""
+	goversion = ""
 )
 
 type options struct {
-	Path string
+	Path         string
+	PrintVersion bool
 }
 
 func main() {
 	opts := parseOpts()
+	if opts.PrintVersion {
+		fmt.Printf("version %s, built by %s with %s ", version, builder, goversion)
+		os.Exit(0)
+	}
 	m, err := manifest.FromFile(opts.Path + "/+MANIFEST")
 	archiver.Archive(opts.Path)
 }
@@ -20,6 +33,7 @@ func main() {
 func parseOpts() *options {
 	ret := &options{}
 	flag.StringVar(&ret.Path, "path", "", "directory to build package from")
+	flag.BoolVar(&ret.PrintVersion, "version", false, "print version and exit")
 
 	flag.Parse()
 	return ret
